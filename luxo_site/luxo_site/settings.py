a68ad55efc4bd1852a828ca64f957184cae99e9d
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-05-30 09:01:13 -0300

    add host

--- luxo_site/luxo_site/settings.py
+++ luxo_site/luxo_site/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'DJANGO_SECRET_KEY', 'your-default'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1' 'ponto-de-luxo.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
